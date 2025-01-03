4379a8c4f821f1d54ddbf89f88979e39e37d7030
katya1nik <katya77nik@gmail.com>
2025-01-03 21:48:38 +0300

    Создала функцию для чтения CSV файла (read_csv)

--- работа с файлами JSON_CSV_TXT_YAML.py
+++ работа с файлами JSON_CSV_TXT_YAML.py
@@ -85,19 +85,18 @@ name_list = [
     ["Петров", "Петр", "Петрович"],
 ]
 
-def write_csv(data, file_path, delimiter=';', encoding: str ='windows-1251') -> None:
-    """
-    Функция записи в csv файл.
-    :param data: данные для записи
-    :param file_path: путь к файлу (в данном случае название файла)
-    :param delimiter: разделитель
-    :param encoding: кодировка
-    """
-    with open(file_path, 'w', encoding=encoding) as file:
-        writer = csv.writer(file, delimiter=delimiter, lineterminator="\n")
-        writer.writerows(name_list)
-write_csv(name_list, file_path='name.csv', delimiter=';', encoding='windows-1251')
-
+# def write_csv(data, file_path, delimiter=';', encoding: str ='utf-8-sig') -> None:
+#     """
+#     Функция записи в csv файл.
+#     :param data: данные для записи
+#     :param file_path: путь к файлу (в данном случае название файла)
+#     :param delimiter: разделитель
+#     :param encoding: кодировка
+#     """
+#     with open(file_path, 'w', encoding=encoding) as file:
+#         writer = csv.writer(file, delimiter=delimiter, lineterminator="\n")
+#         writer.writerows(name_list)
+# write_csv(name_list, file_path='name.csv')
 
 
 # new_name = ["Сидоров", "Сидор", "Сидорович"]
@@ -106,70 +105,25 @@ write_csv(name_list, file_path='name.csv', delimiter=';', encoding='windows-1251
 #     writer = csv.writer(file, delimiter=";", lineterminator="\n")
 #     writer.writerow(name_list)
 
-# with open("name.csv", "r", encoding="utf-8-sig") as file:
-#     reader = csv.reader(file, delimiter=";")
-#     name_list = list(reader)
-
-# print(name_list)
-
-
-# name_dict = [
-#     {"lastname": "Иванов", "firstname": "Иван", "middlename": "Иванович"},
-#     {"lastname": "Петров", "firstname": "Петр", "middlename": "Петрович"},    
-# ]
-
-# with open("name.csv", "w", encoding="utf-8-sig") as file:
-#     writer = csv.DictWriter(
-#         file, fieldnames=name_dict[0].keys(), delimiter=";", lineterminator="\n"
-#     )
-#     writer.writeheader()
-#     writer.writerows(name_dict)
+# Создание функции для чтения CSV
+def read_csv(file_path, delimiter=';', encoding: str ='utf-8-sig')-> list[str]:
+    """
+    Функция чтения csv файла.
+    :param file_path: путь к файлу (в данном случае название файла)
+    :param delimiter: разделитель
+    :param encoding: кодировка
+    """
+    with open(file_path, 'r', encoding=encoding) as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        return list(reader)
+    
 
 
-# new = {"lastname": "Пантелеев", "firstname": "Пантелей", "middlename": "Паетелеевич"}
-
-# with open("name.csv", "a", encoding="utf-8-sig") as file:
-#     writer = csv.DictWriter(
-#         file, fieldnames=name_dict[0].keys(), delimiter=";", lineterminator="\n"
-#     )
-#     writer.writerow(new)
-
-# with open("name.csv", "r", encoding="utf-8-sig") as file:
-#     reader = csv.DictReader(file, delimiter=";")
-#     name_dict = list(reader)
-
-# from pprint import pprint
-# pprint(name_dict, sort_dicts=False, width=100)
-
-# from tabulate import tabulate
-# print(tabulate(name_dict, headers="keys", tablefmt="grid"))
-
-# html_table = tabulate(name_dict, headers="keys", tablefmt="html")
-
-# styled_table = html_table.replace(
-#     '<table>', 
-#     '<table class="table table-striped table-hover">'
-# )
-
-# html_template = f"""
-# <!doctype html>
-# <html lang="ru">
-#     <head>
-#         <meta charset="utf-8">
-#         <meta name="viewport" content="width=device-width, initial-scale=1">
-#         <title>Список имен</title>
-#         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-#     </head>
-#     <body>
-#         <div class="container mt-5">
-#             <h1 class="mb-4">Список имен</h1>
-#             {styled_table}
-#         </div>
-#         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-#     </body>
-# </html>
-# """
+with open("name.csv", "r", encoding="utf-8-sig") as file:
+    reader = csv.reader(file, delimiter=";")
+    name_list = list(reader)
+    
+result = read_csv("name.csv")
+print(result)
 
 
-# with open("name.html", "w", encoding="utf-8") as file:
-#     file.write(html_template)
