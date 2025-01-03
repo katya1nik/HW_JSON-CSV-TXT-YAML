cdd171bb8bfdbaa882029f8df11517f4db627b13
katya1nik <katya77nik@gmail.com>
2025-01-03 13:58:35 +0300

    Поменяла местами расположение функций

--- работа с файлами JSON_CSV_TXT_YAML.py
+++ работа с файлами JSON_CSV_TXT_YAML.py
@@ -9,23 +9,28 @@ json_string = """
 ]
 """
 
-# Преобразование json в список
-result = json.loads(json_string)
-print(result)
-# Преобразование списка в json
-result = json.dumps(result)
-print(result)
+# python_data = json.loads(json_string)
+# print(python_data)
+
+# json_string = json.dumps(python_data, ensure_ascii=False, indent=4)
+# print(json_string)
 
 
+# #  Создание функции для записи в файл json
+# def write_json(*data: str, file_path: str, encoding: str = "utf-8") -> None:
+#     """
+#     Функция записи в json файл.
+#     :param data: данные для записи
+#     :param file_path: путь к файлу (в данном случае название файла)
+#     :param encoding: кодировка
+#     """
+#     with open(file_path, 'w', encoding=encoding) as file:
+#         json.dump(data, file, ensure_ascii=False, indent=4)
 
+# name1="Никулина Екатерина Александровна"
+# name2="Иванов Иван Иванович"
 
-
-
-
-
-
-
-
+# write_json(name1, name2, file_path='name.json')
 
 # Создание функции для чтения json
 
@@ -42,49 +47,32 @@ print(result)
 # print(result)
 
 
-# Создание функции для записи в файл json
-def write_json(*data: dict, file_path: str, encoding: str = "utf-8") -> None:
-    """
-    Функция записи в json файл.
-    :param data: данные для записи
-    :param file_path: путь к файлу (в данном случае название файла)
-    :param encoding: кодировка
-    """
+Создание функции для дозаписи в файл JSON:
+with open('name.json', 'r', encoding='utf-8') as file:
+    python_data = json.load(file)
+
+new_name = "Новое имя5"
+python_data.append(new_name)
+
+with open('name.json', 'w', encoding='utf-8') as file:
+    json.dump(python_data, file, ensure_ascii=False, indent=4)
+
+def append_json(*data: list[dict], file_path: str, encoding: str = 'utf-8', indent: int) -> None:
+    with open(file_path, 'r', encoding=encoding) as file:
+        data = json.load(file)
+
+
+# Проверка типа данных
+    if not isinstance(data, list):
+        raise TypeError("Поддерживается только добавление в JSON-массив")
+
+    data.extend(data)
+
     with open(file_path, 'w', encoding=encoding) as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+        json.dump(data, file, ensure_ascii=False, indent=indent)
 
-name1={"name": "Никулина Екатерина Александровна"}
-name2={"name": "Иванов Иван Иванович"}
-
-write_json(name1, name2, file_path='names_1.json')
-
-
-# Создание функции для дозаписи в файл JSON:
-# with open('name.json', 'r', encoding='utf-8') as file:
-#     python_data = json.load(file)
-
-# new_name = "Новое имя25"
-# python_data.append(new_name)
-
-# with open('name.json', 'w', encoding='utf-8') as file:
-#     json.dump(python_data, file, ensure_ascii=False, indent=4)
-
-# def append_json(*data: list[dict], file_path: str, encoding: str = 'utf-8', indent: int) -> None:
-#     with open(file_path, 'r', encoding=encoding) as file:
-#         data = json.load(file)
-
-
-# # Проверка типа данных
-#     if not isinstance(data, list):
-#         raise TypeError("Поддерживается только добавление в JSON-массив")
-
-#     data.extend(data)
-
-#     with open(file_path, 'w', encoding=encoding) as file:
-#         json.dump(data, file, ensure_ascii=False, indent=indent)
-
-# file = 'name.json'
-# append_json(file, 'Новое имя25', 'Новое имя 6', indent=2)
+file = 'name.json'
+append_json(file, 'Новое имя25', 'Новое имя 6', indent=2)
 
 
 
